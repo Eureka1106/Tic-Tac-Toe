0d1ef15f25dffc498e58d868235a0b0f24c8c642
Eureka1106 <104462011+Eureka1106@users.noreply.github.com>
2022-08-18 03:54:45 +0800

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -15,11 +15,14 @@ pygame.draw.line(surface=main_window, color=(121, 53, 59), start_pos=(400, 0), e
 console_board = numpy.zeros((3, 3))    # 创建初始值为0的3*3数列
 player_number = 1    # 第一个点击的是Player 1
 
+
 def area_choose(row, column, player):
     console_board [row] [column] = player  # 令数列某位置的数字值为1或2（默认为0）
 
+
 def area_available(row, column):    # 判断方格是否可下棋
     return console_board [row] [column] == 0  # 返回布尔值
+
 
 def draw_figures():    # 根据数列的值画圆或叉
     for row in range(0, 3):
@@ -42,29 +45,39 @@ def draw_figures():    # 根据数列的值画圆或叉
                                  end_pos=(column * 200 + 155, row * 200 + 45),
                                  width=24)
 
+
 def check_win(player):    # 判断是否有一方胜利（不只可以返回True，未获胜时也得返回False）并画图
     for row in range(0, 3):
-        if console_board [row] [0] == player and console_board [row] [1] == player and console_board [row] [2] == player:
+        if (console_board [row] [0] == player
+                and console_board [row] [1] == player
+                and console_board [row] [2] == player):
             horizontal_winning(row)    # 在当前行画横线
             return True
     for column in range(0, 3):
-        if console_board [0] [column] == player and console_board [1] [column] == player and console_board [2] [column] == player:
+        if (console_board [0] [column] == player
+                and console_board [1] [column] == player
+                and console_board [2] [column] == player):
             vertical_winning(column)    # 在当前列画竖线
             return True
-    if console_board [2] [0] == player and console_board [1] [1] == player and console_board [0] [2] == player:
+    if (console_board [2] [0] == player
+            and console_board [1] [1] == player
+            and console_board [0] [2] == player):
         ascending_diagonal()
         return True
-    if console_board [0] [0] == player and console_board [1] [1] == player and console_board [2] [2] == player:
+    if (console_board [0] [0] == player
+            and console_board [1] [1] == player
+            and console_board [2] [2] == player):
         descending_diagonal()
         return True
     return False
 
-def board_full():  # 判断是否平局（棋盘是否填满）
-    for row in range(0, 3):
-        for column in range(0, 3):    # 遍历行、列
-            if console_board [row] [column] == 0:
-                return False
-    return True    # 一个空值都没有则返回真
+# def board_full():  # 判断是否平局（棋盘是否填满）
+#     for row in range(0, 3):
+#         for column in range(0, 3):    # 遍历行、列
+#             if console_board [row] [column] == 0:
+#                 return False
+#     return True    # 一个空值都没有则返回真
+
 
 def horizontal_winning(row):
     pygame.draw.line(surface=main_window,
@@ -73,12 +86,14 @@ def horizontal_winning(row):
                      end_pos=(585, row*200+100),
                      width=15)
 
+
 def vertical_winning(column):
     pygame.draw.line(surface=main_window,
                      color=(233, 189, 193),
                      start_pos=(column*200+100, 15),
                      end_pos=(column*200+100, 585),
                      width=15)
+
 
 def ascending_diagonal():    # 画升对角线
     pygame.draw.line(surface=main_window,
@@ -87,12 +102,14 @@ def ascending_diagonal():    # 画升对角线
                      end_pos=(580, 20),
                      width=20)
 
+
 def descending_diagonal():    # 画降对角线
     pygame.draw.line(surface=main_window,
                      color=(233, 189, 193),
                      start_pos=(20, 20),
                      end_pos=(580, 580),
                      width=20)
+
 
 game_running = True
 game_over = False
